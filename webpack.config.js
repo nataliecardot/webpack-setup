8a6826f4b27b0b78133c6e69d1fb5ca41318ae68
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-29 12:39:55 -0700

    Set up html-webpack-plugin and html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
+// Among other things, html-webpack-plugin generates index.html using the version in src, but adds a script link to output main.js file, and html-loader (with the minimize option set to true) minimizes the HTML
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+  ]
+}
