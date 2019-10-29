82a13c475c7481445abe3eadf81ee4a7ab75ce6c
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-29 14:23:09 -0700

    Install file-loader and add rule for it

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
           }
         ]
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
     ]
   },
   plugins: [
