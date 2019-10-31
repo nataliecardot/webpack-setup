db1d23b71e7c2e04d5877dfdc52c41d2943f331e
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-30 22:35:40 -0700

    Add plugin and rule for Sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // Among other things, html-webpack-plugin generates index.html using the version in src, but adds a script link to output main.js file, and html-loader (with the minimize option set to true) minimizes the HTML
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   module: {
@@ -26,6 +27,14 @@ module.exports = {
           'file-loader'
         ]
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ]
   },
   plugins: [
@@ -33,5 +42,11 @@ module.exports = {
       template: './src/index.html',
       filename: './index.html'
     }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
   ]
 }
